cff046f53de2a869bcb53e203335d4d7a8c4da58
Aquiles Aguiar <aquilesvibe@gmail.com>
2021-08-22 16:05:30 -0300

    Base
    Começo da estruturação do template, falta bonitesa.

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,url_for
 import requests
 
 app = Flask(__name__)
 
-url = 'https://api.hgbrasil.com/weather?key=00764386&user_ip=remote'
+url = 'https://api.hgbrasil.com/weather?key=00764386&city_name=Serra,ES'
+
 
 @app.route('/')
 def index():
     req = requests.get(url).json()
     req = req['results']
-    return render_template("index.html",tempo = req)
+    cod_img = req['img_id']
+    img = f'http://assets.api.hgbrasil.com/weather/images/{cod_img}.png'
+
+    # Json com apenas o primeiro indice da lista
+    tempo1 = req["forecast"][0]
+    # Retira o primeiro indice
+    req["forecast"].pop(0)
+    # Pega a lista de json -1 indice
+    tempo2 = req["forecast"]
+    return render_template("index.html",tempo = req,tempo_img = img,tempo1=tempo1,tempo2=tempo2)
 
 
 
