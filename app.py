784aa039187d78c9dc84f713cd51421dbb0fb8ee
Matheus Oliveira <teteuoliveira12@gmail.com>
2021-08-28 18:10:40 -0300

    Funcionou caralho

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
 from flask import Flask, render_template, request,url_for
 import requests
+from pprint import pprint
+from pyA20.gpio import gpio
+from pyA20.gpio import port
+
+led = port.PA12
+
+led2 = port.PA11
+
+print(led2)
+
+gpio.init()
+
+# gpio.setcfg(led, gpio.OUTPUT)
+
+gpio.setcfg(led2, gpio.OUTPUT)
+
+# Resetando as variaveis
+
 
 app = Flask(__name__)
 
@@ -12,6 +30,8 @@ def index():
     req = req['results']
     cod_img = req['img_id']
     img = f'http://assets.api.hgbrasil.com/weather/images/{cod_img}.png'
+    print("========== BUSCANDO API ==========")
+    pprint(req)
 
     # Json com apenas o primeiro indice da lista
     tempo1 = req["forecast"][0]
@@ -19,14 +39,40 @@ def index():
     req["forecast"].pop(0)
     # Pega a lista de json -1 indice
     tempo2 = req["forecast"]
-    return render_template("index.html",tempo = req,tempo_img = img,tempo1=tempo1,tempo2=tempo2)
+    return render_template("index.html",tempo = req, tempo_img = img, tempo1= tempo1 ,tempo2= tempo2 )
 
 
-
-@app.route('/api/lampada')
+@app.route('/lampada')
 def lampada():
-    return "Lampada"
+    return '''<a class="button" href="{{url_for('lampadaOn')}}">Ligar</a> <br> <a class="button" href="{{url_for('lampadaOff')}}">Desligar</a>'''
+
+@app.route("/api/lampanda")
+def lampada_api():
+    status = request.args.get("status")
+
+    if not status:
+        print("Erro de Rota")
+        return "Erro faltando parametros"
+
+    if status == "on":
+        print("Ligando a Lampada")
+        gpio.output(led, 1)
+        return "Lampada Ligada"
+
+    if status == "off":
+        print("Desligando a Lampada")
+        gpio.output(led, 0)
+        return "Lampada Desligada"
+
+@app.route('/lampada/on')
+def lampadaOn():
+    gpio.output(led, 1)
+    return '''LampadaOn <a class="button" href="{{url_for('lampadaOn')}}">Ligar</a> <br> <a class="button" href="{{url_for('lampadaOff')}}">Desligar</a>'''
+
+@app.route('/lampada/off')
+def lampadaOff():
+    return '''LampadaOff <a class="button" href="{{url_for('lampadaOn')}}">Ligar</a> <br> <a class="button" href="{{url_for('lampadaOff')}}">Desligar</a>'''
 
 
 if __name__ == '__main__':
-    app.run(debug=True,use_reloader=True)
+    app.run( host='0.0.0.0', port=8080, debug=True )
