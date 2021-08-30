2a50d5e85dab9f802e88853c282971ae4ead7bdf
Aquiles Aguiar <aquilesvibe@gmail.com>
2021-08-30 18:29:09 -0300

    App.py

--- app/app.py
+++ app/app.py
@@ -43,4 +43,5 @@ def lampadaOff():
 
 
 if __name__ == '__main__':
-    app.run( host='0.0.0.0', port=8080, debug=True )
+    app.run(debug=True)
+    # app.run( host='0.0.0.0', port=8080, debug=True )
