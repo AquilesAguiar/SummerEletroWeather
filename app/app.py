5f18837cdc8aa92c01a4fc5a669b021c2d31a161
Aquiles Aguiar <aquilesvibe@gmail.com>
2021-08-30 19:54:50 -0300

    Controller tempo

--- app/app.py
+++ app/app.py
@@ -1,31 +1,16 @@
 from flask import Flask, render_template, request,url_for,redirect
-import requests
-from pprint import pprint
-
-
-
-
-# Resetando as variaveis
-
+from controllers.tempo import Tempo
+# from controllers import led
 
 app = Flask('SummerEltroWeather')
 
-url = 'https://api.hgbrasil.com/weather?key=00764386&city_name=Serra,ES'
-
-
 @app.route('/')
 def index():
-    req = requests.get(url).json()
-    req = req['results']
-    cod_img = req['img_id']
-    img = f'http://assets.api.hgbrasil.com/weather/images/{cod_img}.png'
-    
-    tempoProxDias = req["forecast"]
-    
-    return render_template("index.html",tempo = req, tempo_img = img, tempoProxDias = tempoProxDias)
-
-
-
+    clima = Tempo()
+    tempo = clima.getTempo()
+    img = clima.getFotoTempo()
+    tempoProxDias = clima.getProxTempo()
+    return render_template("index.html",tempo = tempo, tempo_img = img, tempoProxDias = tempoProxDias)
 
 @app.route('/lampada/on')
 def lampadaOn():
--- app/controllers/tempo.py
+++ app/controllers/tempo.py
@@ -1,2 +1,19 @@
-class tempo():
+import requests
+class Tempo():
     def __init__(self):
+        self.url = 'https://api.hgbrasil.com/weather?key=00764386&user_ip=remote'
+        self.req = requests.get(self.url).json()
+    
+    def getTempo(self):
+        return self.req['results']
+
+    def getFotoTempo(self):
+        req = self.getTempo()
+        cod_img = req['img_id']
+        img = 'http://assets.api.hgbrasil.com/weather/images/{0}.png'.format(cod_img)
+        return img
+    
+    def getProxTempo(self):
+        req = self.getTempo()
+        tempoProxDias = req["forecast"]
+        return tempoProxDias
