45d25145ab43f55e65bd46e849dee1daa336a6fe
Caio Machado de Souza Gome <caiomachadodesouza1@gmail.com>
2021-09-02 14:49:01 -0300

    Tentativa de teste dos LEDs

--- app/controllers/led.py
+++ app/controllers/led.py
@@ -1,19 +1,43 @@
-from pyA20.gpio import gpio
-from pyA20.gpio import port
+from rpi_ws281x import PixelStrip, Color
+import argparse
 
 class led():
-    def __init__(self):
-        # led = port.PA12
+    def __init__(self, led_count : int, led_pin : int, led_freq_hz : int, led_dma : int, led_brightness : int, led_invert : bool, led_channel : int):
+        self.LED_COUNT = led_count           # Número de LEDs da fita
+        self.LED_PIN = led_pin               # Pino GPIO onde o a fita está conectada (PWM e SPI)
+        self.LED_FREQ_HZ = led_freq_hz       # Frequência de sinal do LED em hertz (Normalmente 800khz)
+        self.LED_DMA = led_dma               # Canal DMA usado para geração do sinal (Sei lá tenta o 10)
+        self.LED_BRIGHTNESS = led_brightness # Quanto mais próximo do 0 mais escuro e mais claro é proximo do 255
+        self.LED_INVERT = led_invert         # True para inverter o sinal (Apenas quando usar um NPN transistor level shift)
+        self.LED_CHANNEL = led_channel       # Altere para 1 para as GPIOs (13, 19, 41, 45 ou 53)
 
-        # led2 = port.PA11
+        def oneCollor(strip, color : Color) -> None:
+            for x in range(0, self.LED_COUNT):
+                strip.setPixelColor(x, color)
+            
+            strip.show()
 
-        # print(led2)
+if __name__ == '__main__':
+    fitaLed = led(60, 18, 8000, 10, 255, False, 0)
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
+    args = parser.parse_args()
 
-        # gpio.init()
+    strip = PixelStrip(fitaLed.LED_COUNT, fitaLed.LED_PIN, fitaLed.LED_FREQ_HZ, fitaLed.LED_DMA, fitaLed.LED_INVERT, fitaLed.LED_BRIGHTNESS, fitaLed.LED_CHANNEL)
+    # Inicializa a biblioteca
+    strip.begin()
 
-        # gpio.output(led, 1)
-        # gpio.output(led, 0)
-        # # gpio.setcfg(led, gpio.OUTPUT)
+    print('Press Ctrl-C to quit.')
+    if not args.clear:
+        print('Use "-c" argument to clear LEDs on exit')
 
-        # gpio.setcfg(led2, gpio.OUTPUT)
-        pass
+    try:
+
+        while True:
+            print('Ligando em uma cor')
+            fitaLed.oneCollor(strip, Color(0, 255, 0))
+
+    except KeyboardInterrupt:
+        if args.clear:
+            fitaLed.oneCollor(strip, Color(0, 0, 0))
